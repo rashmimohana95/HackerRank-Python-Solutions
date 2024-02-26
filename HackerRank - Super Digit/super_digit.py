29f42f2a28a247d45734906b0d93622fc6ab9afa
rashmimohana95 <156934946+rashmimohana95@users.noreply.github.com>
2024-02-26 15:13:51 +0100

    Create super_digit.py

---
+++ HackerRank - Super Digit/super_digit.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superDigit' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING n
+#  2. INTEGER k
+#
+
+def superDigit(n, k):
+    # Write your code here
+    digitlist = []
+    for i in n:
+        digitlist.append(int(i)*k)
+    new = str(sum(digitlist))
+    if len(new) > 1:
+        result = superDigit(new, 1)
+        return result
+    else:
+        return new
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = first_multiple_input[0]
+
+    k = int(first_multiple_input[1])
+
+    result = superDigit(n, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
